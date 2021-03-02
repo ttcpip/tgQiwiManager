3735349e8676bbdf4e6c25796f88c172f9086e54
ttcpip <38619302+ttcpip@users.noreply.github.com>
2021-03-02 10:56:53 +0600

    /leave base cmd added

--- src/bot/middlewares/baseCmdsHandler.js
+++ src/bot/middlewares/baseCmdsHandler.js
@@ -6,13 +6,25 @@ module.exports = async function baseCmdsHandler(ctx, next) {
   const msgText = ctx.message?.text || ''
   switch (msgText) {
   case '/chat':
-    return ctx.reply(ctx.chat?.id || 0)
+    return await ctx.reply(ctx.chat?.id || 0)
   case '/id':
-    return ctx.reply(ctx.from?.id || 0)
+    return await ctx.reply(ctx.from?.id || 0)
 
   default:
     break
   }
 
-  return await next()
+  /**
+   * Handling /leave command because this middleware applies
+   * before scenes middleware, it means that
+   * before 'await next()' ctx.scene is undefined
+   */
+  return await next().then((smth) => {
+    if (ctx.message?.text === '/leave' && ctx.scene && ctx.scene.leave) {
+      return ctx.scene.leave()
+        .then(() => ctx.reply(`Вышел с текущей сцены`))
+        .catch(() => {})
+    }
+    return smth
+  })
 }
