e4badb7db2799975ddd7d515b06b4f82d2dabe12
ttcpip <38619302+ttcpip@users.noreply.github.com>
2021-03-04 19:58:56 +0600

    MINUTE_AS_SECONDS renamed to MINUTE_AS_MS

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const MINUTE_AS_SECONDS = 60 * 1000
+const MINUTE_AS_MS = 60 * 1000
 
 module.exports = {
   tgBotToken: process.env.TG_BOT_TOKEN,
-  autoWithdrawCheckBalanceIntervalMs: MINUTE_AS_SECONDS,
+  autoWithdrawCheckBalanceIntervalMs: MINUTE_AS_MS,
 }
