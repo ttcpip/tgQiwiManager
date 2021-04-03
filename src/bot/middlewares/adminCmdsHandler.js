235f4ddfe23efb81a4bfb31b5f79a97665a589b4
ttcpip <38619302+ttcpip@users.noreply.github.com>
2021-04-03 17:21:09 +0600

    added error handler at adminCmdsHandler()

--- src/bot/middlewares/adminCmdsHandler.js
+++ src/bot/middlewares/adminCmdsHandler.js
@@ -23,110 +23,116 @@ const helpMessageText = dedent`
  * @param {Function} next
  */
 module.exports = async function adminCmdsHandler(ctx, next) {
-  const msgText = ctx.message?.text || ''
-  const lines = msgText.split('\n')
-  const [command, arg1, arg2, arg3] = lines[0].split(' ')
+  try {
+    const msgText = ctx.message?.text || ''
+    const lines = msgText.split('\n')
+    const [command, arg1, arg2, arg3] = lines[0].split(' ')
 
-  if (command === '/help')
-    return await ctx.replyWithMarkdownV2(helpMessageText)
-  if (command === '/adminList')
-    return await ctx.reply(`Админы:\n${settings.data.tgAdminChatIds.join('\n')}`)
-  if (command === '/adminAdd' && arg1) {
-    const uid = parseInt(arg1, 10)
-    if (!Number.isFinite(uid))
-      return await ctx.reply(`Айди введено неверно`)
-    if (settings.data.tgAdminChatIds.includes(uid))
-      return await ctx.reply(`Он уже админ`)
+    if (command === '/help')
+      return await ctx.replyWithMarkdownV2(helpMessageText)
+    if (command === '/adminList')
+      return await ctx.reply(`Админы:\n${settings.data.tgAdminChatIds.join('\n')}`)
+    if (command === '/adminAdd' && arg1) {
+      const uid = parseInt(arg1, 10)
+      if (!Number.isFinite(uid))
+        return await ctx.reply(`Айди введено неверно`)
+      if (settings.data.tgAdminChatIds.includes(uid))
+        return await ctx.reply(`Он уже админ`)
 
-    settings.data.tgAdminChatIds.push(uid)
-    await settings.save()
+      settings.data.tgAdminChatIds.push(uid)
+      await settings.save()
 
-    return await ctx.reply(`Успешно добавлен админ ${uid}`)
-  }
-  if (command === '/adminDel' && arg1) {
-    const uid = parseInt(arg1, 10)
-    if (!Number.isFinite(uid))
-      return await ctx.reply(`Айди введено неверно`)
-    const index = settings.data.tgAdminChatIds.indexOf(uid)
-    if (index < 0)
-      return await ctx.reply(`Он и так не админ`)
-
-    settings.data.tgAdminChatIds.splice(index, 1)
-    await settings.save()
-
-    return await ctx.reply(`Успешно удален админ ${uid}`)
-  }
-  if (command === '/exec' && lines.length > 1) {
-    const jsCode = lines.slice(1).join('\n')
-    // eslint-disable-next-line no-eval
-    const output = await eval(`(async () => { ${jsCode} })()`)
-
-    const outputType = arg1 || 'json'
-    let message = null
-    switch (outputType) {
-    case 'json':
-      message = JSON.stringify(output, getCircularReplacer(), 2)
-      break
-
-    case 'inspect':
-      message = util.inspect(output, {
-        showHidden: true,
-        depth: 5,
-      })
-      break
-
-    case 'pretty':
-      message = `${output}`
-      break
-
-    default:
-      break
+      return await ctx.reply(`Успешно добавлен админ ${uid}`)
     }
-    const isJson = outputType === 'json'
+    if (command === '/adminDel' && arg1) {
+      const uid = parseInt(arg1, 10)
+      if (!Number.isFinite(uid))
+        return await ctx.reply(`Айди введено неверно`)
+      const index = settings.data.tgAdminChatIds.indexOf(uid)
+      if (index < 0)
+        return await ctx.reply(`Он и так не админ`)
 
-    try {
-      return await ctx.reply(`${message && message.length ? message : 'No output'}`)
-    } catch (err) {
-      const filename = `output_${Date.now()}.${isJson ? 'json' : 'txt'}`
-      const source = Buffer.from(message, 'utf8')
-      const caption = `Too long output`
+      settings.data.tgAdminChatIds.splice(index, 1)
+      await settings.save()
 
-      return await ctx.replyWithDocument({
-        filename,
-        source,
-      }, { caption })
+      return await ctx.reply(`Успешно удален админ ${uid}`)
     }
-  }
+    if (command === '/exec' && lines.length > 1) {
+      const jsCode = lines.slice(1).join('\n')
+      // eslint-disable-next-line no-eval
+      const output = await eval(`(async () => { ${jsCode} })()`)
 
-  if (command === '/domainList') {
-    const qiwiDomainsText = settings.data.qiwiDomains
-      .map((domain, i) => `${i + 1}\\) ${monospace(String(domain))}`)
-      .join('\n')
-    return await ctx.replyWithMarkdownV2(`Киви домены:\n${qiwiDomainsText}`)
-  }
-  if (command === '/domainAdd') {
-    const domain = arg1
-    const index = settings.data.qiwiDomains.indexOf(domain)
-    if (index >= 0)
-      return await ctx.reply(`Домен уже есть в списке`)
-    if (!domain)
-      return await ctx.reply(`Некорректно введен домен`)
+      const outputType = arg1 || 'json'
+      let message = null
+      switch (outputType) {
+      case 'json':
+        message = JSON.stringify(output, getCircularReplacer(), 2)
+        break
 
-    settings.data.qiwiDomains.push(domain)
-    await settings.save()
+      case 'inspect':
+        message = util.inspect(output, {
+          showHidden: true,
+          depth: 5,
+        })
+        break
 
-    return await ctx.replyWithMarkdownV2(`Домен добавлен: ${monospace(domain)}`)
-  }
-  if (command === '/domainDel') {
-    const domain = arg1
-    const index = settings.data.qiwiDomains.indexOf(domain)
-    if (index < 0)
-      return await ctx.reply(`Домен для удаления не найден`)
+      case 'pretty':
+        message = `${output}`
+        break
 
-    settings.data.qiwiDomains.splice(index, 1)
-    await settings.save()
+      default:
+        break
+      }
+      const isJson = outputType === 'json'
 
-    return await ctx.replyWithMarkdownV2(`Домен удален: ${monospace(domain)}`)
+      try {
+        return await ctx.reply(`${message && message.length ? message : 'No output'}`)
+      } catch (err) {
+        const filename = `output_${Date.now()}.${isJson ? 'json' : 'txt'}`
+        const source = Buffer.from(message, 'utf8')
+        const caption = `Too long output`
+
+        return await ctx.replyWithDocument({
+          filename,
+          source,
+        }, { caption })
+      }
+    }
+
+    if (command === '/domainList') {
+      const qiwiDomainsText = settings.data.qiwiDomains
+        .map((domain, i) => `${i + 1}\\) ${monospace(String(domain))}`)
+        .join('\n')
+      return await ctx.replyWithMarkdownV2(`Киви домены:\n${qiwiDomainsText}`)
+    }
+    if (command === '/domainAdd') {
+      const domain = arg1
+      const index = settings.data.qiwiDomains.indexOf(domain)
+      if (index >= 0)
+        return await ctx.reply(`Домен уже есть в списке`)
+      if (!domain)
+        return await ctx.reply(`Некорректно введен домен`)
+
+      settings.data.qiwiDomains.push(domain)
+      await settings.save()
+
+      return await ctx.replyWithMarkdownV2(`Домен добавлен: ${monospace(domain)}`)
+    }
+    if (command === '/domainDel') {
+      const domain = arg1
+      const index = settings.data.qiwiDomains.indexOf(domain)
+      if (index < 0)
+        return await ctx.reply(`Домен для удаления не найден`)
+
+      settings.data.qiwiDomains.splice(index, 1)
+      await settings.save()
+
+      return await ctx.replyWithMarkdownV2(`Домен удален: ${monospace(domain)}`)
+    }
+  } catch (err) {
+    console.error(`Err at adminCmdsHandler():`)
+    console.error(err)
+    return await ctx.reply(`Ошибка при обработке команды: ${err.message || err.description || '[no err text]'}`).catch(() => {})
   }
 
   return await next()
