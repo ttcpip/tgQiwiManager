e820687a87a9f93898e4b7605265480b44c07c9f
ttcpip <38619302+ttcpip@users.noreply.github.com>
2022-07-30 00:30:07 +0600

    feat(googleapis): checkRefreshToken every minute

--- src/lib/googleapis/index.js
+++ src/lib/googleapis/index.js
@@ -1,4 +1,6 @@
 const { google } = require('googleapis')
+const writeFile = require('write-file-atomic')
+const fs = require('fs')
 const credentials = require('./credentials.json')
 const token = require('./token.json')
 
@@ -10,46 +12,20 @@ oAuth2Client.setCredentials(token)
 
 const sheets = google.sheets({ version: 'v4', auth: oAuth2Client })
 
+const TOKEN_FILE_PATH = './src/lib/googleapis/token.json'
+async function checkRefreshToken() {
+  try {
+    const token = JSON.parse(await fs.promises.readFile(TOKEN_FILE_PATH, 'utf8'))
+    if (!token.expiry_date) throw new Error(`!token.expiry_date`)
+    if (token.expiry_date > Date.now()) return
+
+    const { credentials: newToken } = await oAuth2Client.refreshAccessToken()
+    if (!newToken) throw new Error(`!newToken`)
+    await writeFile(TOKEN_FILE_PATH, JSON.stringify(newToken, null, 2))
+  } catch (err) {
+    console.error(`When updating googleapis access_token:`, err)
+  }
+}
+setInterval(checkRefreshToken, 1 * 60 * 1000)
+
 module.exports = { sheets }
-
-// sheets.spreadsheets.values.batchUpdate({
-//   spreadsheetId: '1wfFpovjaseVPamRt8n1Pa5qSnaHKHEGh9D3YF2Xehus',
-//   requestBody: {
-//     valueInputOption: 'USER_ENTERED', // RAW
-//     data: [
-//       {
-//         range: 'A20:A23',
-//         majorDimension: 'ROWS',
-//         values: [
-//           ['Прокси'],
-//           ['Лимит'],
-//           ['Бан'],
-//           ['Ворк'],
-//         ],
-//       },
-//     ],
-//   },
-// }, (err, res) => {
-//   if (err)
-//     return console.error('The API returned an error: ', err)
-
-//   console.log(res.data)
-// })
-
-// ---------------------------
-
-// sheets.spreadsheets.values.get({
-//   spreadsheetId: '1wfFpovjaseVPamRt8n1Pa5qSnaHKHEGh9D3YF2Xehus',
-//   range: 'A6:G10',
-// }, (err, res) => {
-//   if (err)
-//     return console.error('The API returned an error: ', err)
-//   const rows = res.data.values
-//   if (rows.length) {
-//     console.log('Name, Major:')
-//     // Print columns A and E, which correspond to indices 0 and 4.
-//     rows.map((row) => console.log(row.join(', ')))
-//   } else {
-//     console.log('No data found.')
-//   }
-// })
